2dd627bb511f6d3b2b6dc36cc8e99732a9e4259f
yili <eli.he999@hotmail.com>
2019-01-16 14:43:31 +0800

    Update test1.py

--- base/re/test1.py
+++ base/re/test1.py
@@ -1,16 +1,20 @@
+#!/usr/bin/env python
+# Filename: test1.py
+# -*- coding: utf-8 -*-
+
 import re
 
 print(re.match('www', 'www.google.com.cn').span())
 
-line = "Cat are smarter than dogs"
-matchObj = re.match( r'(.*) are (.*?) .*', line, re.M|re.I )
+Line = "Cat are smarter than dogs"
+MatchObj = re.match( r'(.*) are (.*?) .*', Line, re.M|re.I )
 
-if matchObj:
-    print("group()", matchObj.group())
-    print("group(0)", matchObj.group(0))
-    print("group  ", matchObj)
-    print("group(1)", matchObj.group(1))
-    print("group(2)", matchObj.group(2))
-    # print("group(3) error", matchObj.group(3))
+if MatchObj:
+    print("group()", MatchObj.group())
+    print("group(0)", MatchObj.group(0))
+    print("group  ", MatchObj)
+    print("group(1)", MatchObj.group(1))
+    print("group(2)", MatchObj.group(2))
+    # print("group(3) error", MatchObj.group(3))
 else:
     print("no match")
