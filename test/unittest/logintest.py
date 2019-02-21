9913fe6386490b6f669ed5b612fb3a513c0f2357
yili <eli.he999@hotmail.com>
2019-02-21 16:27:37 +0800

    <chore>Update unittest

--- test/unittest/logintest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import requests
-
-class logintest(unittest.TestCase):
-    def testlogin(self):
-        url = "http://xx/login.html"
-        form = {"username":123}
-        r = requests.post(url, data = form)
-        self.assertEqual(r.text, "login success")
